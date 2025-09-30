3bba37a73bbbbb8bc5487ad1341e185ff360dfae
adlinare <caos1234matao@gmail.com>
2025-09-30 14:24:42 +0200

    Corrección de error con el enunciado

--- EjercicioTercerdia/Ejercicio3-7/Ejercicio3-7/Program.cs
+++ EjercicioTercerdia/Ejercicio3-7/Ejercicio3-7/Program.cs
@@ -16,11 +16,15 @@ namespace Ejercicio3_7
             int num2 = int.Parse(Console.ReadLine());
             if (SonMultiplos(num1, num2))
             {
-                Console.WriteLine("El primer numero es multiplo del segundo");
+                Console.WriteLine($"El numero {num1} es multiplo del {num2}");
+            }
+            else if (SonMultiplos(num2, num1))
+            {
+                Console.WriteLine($"El numero {num2} es multiplo del {num1}");
             }
             else
             {
-                Console.WriteLine("El primer numero es multiplo del segundo");
+                Console.WriteLine($"El numero {num1} y {num2} no son multiplos");
             }
         }
         public static bool SonMultiplos(int num1, int num2)
@@ -34,4 +38,5 @@ namespace Ejercicio3_7
                 return false;
             }
         }
+    }
 }
