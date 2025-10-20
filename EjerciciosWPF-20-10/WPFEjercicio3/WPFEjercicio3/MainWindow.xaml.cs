0a1fa43bcfc32f0ec501b88c2074f0a69a0f259d
adlinare <caos1234matao@gmail.com>
2025-10-20 10:52:25 +0200

    Ejercicios wpf

---
+++ EjerciciosWPF-20-10/WPFEjercicio3/WPFEjercicio3/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEjercicio3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button bt = new Button();
+            bt.FontWeight = FontWeights.Bold;
+            bt.Height = 65;
+            bt.Width = 90;
+            bt.Content = new WrapPanel
+            {
+                Children =
+                {
+                    new TextBlock
+                    {
+                        Text = "Click me",
+                        Foreground = Brushes.Green
+                    },
+                    new TextBlock
+                    {
+                        Text = "Click me",
+                        Foreground = Brushes.Blue
+                    },
+                    new TextBlock
+                    {
+                        Text = "Click me"
+                    }
+                }
+            };
+            MainGridid.Children.Add(bt);
+
+        }
+    }
+}
